92c89e07e49578d9dd07b5fdfe20faba591f9022
zack <zack.bitcoin@gmail.com>
2021-08-19 21:01:22 -0400

    now it can calculate distance and area.

--- rationals.erl
+++ rationals.erl
@@ -5,7 +5,17 @@
          gps_to_rat/2,
          rat_to_gps/1,
          simplify/1,
+         angle/2, distance/2,
          meets/2, test/0]).
+
+-define(radius, 6371000). 
+
+%for a triangle on a sphere.
+%angle1 + angle2 + angle3 - pi = excess angle.
+%excess angle * r^2 is area.
+
+%to calculate angles, consider the angle between the planes. which is the angle between the normal to each plane.
+
 
 %if a point is on a line, then
 %ax + by + zc = 0
@@ -46,8 +56,12 @@ rat_to_gps(P) when is_record(P, point) ->
 gps_to_rat(Lat0, Long0) when (Lat0 < 0) ->
     gps_to_rat(-Lat0, Long0 + 180);
 gps_to_rat(Lat0, Long0) ->
+    Long1 = if
+                Long0 > 180 -> Long0 - 180;
+                true -> Long0
+            end,
+    Long = Long1 / 180 * math:pi(),
     Lat = Lat0 / 180 * math:pi(),
-    Long = (Long0 rem 360) / 180 * math:pi(),
     %lat is between -90 and 90. 90 is north pole.
     %long is between -180 and 180. 0 is London.
     %the line through the north pole and london is lat=0. includes points {0, 1, 1} and {0, 2, 1}
@@ -94,7 +108,7 @@ test() ->
     Equator = {line, 0, 0, 1},%circle at infinity
     Main = intersection(LondonLat, Equator),
 
-    {LondonLat, Equator, Main},
+    %{LondonLat, Equator, Main},
     
     {rat_to_gps(gps_to_rat(0,0)),
      rat_to_gps(gps_to_rat(0,25)),
@@ -109,7 +123,16 @@ test() ->
      (gps_to_rat(15,210)),%B
      (gps_to_rat(-15,30)),%B
      (gps_to_rat(-15,210))%A
-    }.
+    },
+    
+    {distance(gps_to_rat(1, 1), 
+              gps_to_rat(1, 1.0001))
+    },
+
+    {area(gps_to_rat(0.01, 0.01),
+          gps_to_rat(0, 0.01),
+          gps_to_rat(0.01, 0))}.
+    
 
      
 
@@ -162,18 +185,15 @@ meets(P1, L1) when
       (is_record(L1, line) and
        is_record(P1, point)) ->
     meets(L1, P1).
-%central_line(L1) ->
-%    L1#line.c == 0.
-is_parallel(L1, L2) ->
-    L1#line.a*L2#line.b ==
-        L1#line.b * L2#line.a.
 is_perpendicular(L1, L2) -> %in blue geometry
+    %unused
     (L1#line.a * L1#line.b) +
         (L2#line.a * L2#line.b)
         == 0.
-equal(X1, X2) 
+equal(X1, X2)
   when (is_record(X1, point) 
         and is_record(X2, point)) ->
+    %unused
     A1 = X1#point.x,
     A2 = X1#point.y,
     A3 = X1#point.z,
@@ -184,6 +204,7 @@ equal(X1, X2)
 equal(X1, X2) when is_record(X1, line) ->
     equal(dual(X1), dual(X2)).
 proportion_equal(A1, B1, C1, A2, B2, C2) ->
+    %unused
     A3 = (A1 * B2) - (A2 * B1),
     B3 = (B1 * C2) - (C1 * B2),
     C3 = (C1 * A2) - (C2 * A1),
@@ -198,6 +219,9 @@ dual(P) when is_record(P, point) ->
     #line{a = P#point.x,
           b = P#point.y,
           c = P#point.z}.
+is_parallel(L1, L2) ->
+    L1#line.a*L2#line.b ==
+        L1#line.b * L2#line.a.
 intersection(L1, L2) when (is_record(L1, line) and
                            is_record(L2, line))->
     %io:fwrite("intersection\n"),
@@ -225,23 +249,15 @@ intersection(P1, P2) when (is_record(P1, point) and
                    is_record(P2, point)) ->
     dual(intersection(dual(P1), dual(P2))).
 collinear(P1, P2, P3) ->
+    %unused
     L = intersection(P1, P2),
     meets(L, P3).
 concurrent(L1, L2, L3) ->
+    %unused
     P = intersection(L1, L2),
     meets(L3, P).
-parallel_to(L, P) ->
-    A = L#line.a,
-    B = L#line.b,
-    %C = L#line.c,
-    X = P#point.x,
-    Y = P#point.y,
-    Z = P#point.z,
-    L2 = #line{a = A*Z,
-               b = B*Z,
-               c = -((A*X) + (B*Y))},
-    simplify(L2).
 altitude_to(L, P) ->
+    %unused
     A = L#line.a,
     B = L#line.b,
     %C = L#line.c,
@@ -253,9 +269,11 @@ altitude_to(L, P) ->
                c = ((B*X)-(A*Y))},
     simplify(L2).
 altitude_foot(L, P) ->
+    %unused
     A = altitude_to(L, P),
     intersection(A, L).
 mid_point(P1, P2) ->
+    %unused
     X1 = P1#point.x,
     Y1 = P1#point.y,
     Z1 = P1#point.z,
@@ -267,7 +285,46 @@ mid_point(P1, P2) ->
                 z = 2*Z1*Z2},
     simplify(P3).
 perpendicular_bisector(P1, P2) ->
+    %unused
     L1 = intersection(P1, P2),
     M = mid_point(P1, P2),
     A = altitude_to(L1, M),
     simplify(A).
+
+angle(P1, P2) ->
+    %cos = u dot v / ||u|| * ||v||
+    X1 = P1#point.x,
+    X2 = P2#point.x,
+    Y1 = P1#point.y,
+    Y2 = P2#point.y,
+    Z1 = P1#point.z,
+    Z2 = P2#point.z,
+    T = (X1 * X2) + (Y1 * Y2) + (Z1 * Z2),
+    B1 = (X1 * X1) + (Y1 * Y1) + (Z1 * Z1),
+    B2 = (X2 * X2) + (Y2 * Y2) + (Z2 * Z2),
+    B3 = math:sqrt(B1 * B2),
+    min(math:acos(T/B3),
+        math:acos(-T/B3)).
+
+area(P1, P2, P3) ->
+
+    A1 = angle(P2, P3),
+    A2 = angle(P1, P3),
+    A3 = angle(P1, P2),
+   
+    T = math:cos(A1) - (math:cos(A2)*math:cos(A3)),
+    B = math:sin(A2)*math:sin(A3),
+    
+    SA1 = math:acos(T/B),
+    
+    %math:sin(SA1) / math:sin(A1) = math:sin(SA2) / math:sin(A2)
+    SA2 = math:asin((math:sin(SA1)*math:sin(A2))/math:sin(A1)),
+    SA3 = math:asin((math:sin(SA1)*math:sin(A3))/math:sin(A1)),
+
+    Excess = SA1 + SA2 + SA3 - math:pi(),
+    Excess * ?radius * ?radius.%in square meters
+    
+
+distance(P1, P2) ->
+    A = angle(P1, P2),
+    A * ?radius.
